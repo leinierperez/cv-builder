06d6148462f86e05320dd4a107ef38ab1cc6022d
Leinier Perez <P_leinier@yahoo.com>
2022-12-10 09:43:16 -0500

    Add the general info section and handle keyboard input on main component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
 
 class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
-    return <div></div>;
+    const { firstName, lastName, email, phone, title, address, description } =
+      this.props.generalInfo;
+
+    return (
+      <section>
+        <div className='section-header'>
+          <h2>General Info</h2>
+        </div>
+        <input
+          name='firstName'
+          type='text'
+          value={firstName}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='First Name'
+        />
+        <input
+          name='lastName'
+          type='text'
+          value={lastName}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Last Name'
+        />
+        <input
+          name='email'
+          type='email'
+          value={email}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Email'
+        />
+        <input
+          name='phone'
+          type='text'
+          value={phone}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Phone Number'
+        />
+        <input
+          name='title'
+          type='text'
+          value={title}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Title'
+        />
+        <input
+          name='address'
+          type='text'
+          value={address}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Address'
+        />
+        <textarea
+          name='description'
+          value={description}
+          onChange={(event) => this.props.handleChange(event, 'generalInfo')}
+          placeholder='Description'
+        />
+      </section>
+    );
   }
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,37 @@ import GeneralInfo from './GeneralInfo';
 class Main extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      generalInfo: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        title: '',
+        address: '',
+        description: '',
+      },
+    };
   }
+
+  handleInputChange = (event, section) => {
+    const value = event.target.value;
+    this.setState({
+      [section]: {
+        ...this.state[section],
+        [event.target.name]: value,
+      },
+    });
+  };
 
   render() {
     return (
       <div>
-        <GeneralInfo />
-        <Education />
-        <Experience />
+        <GeneralInfo
+          handleChange={this.handleInputChange}
+          generalInfo={this.state.generalInfo}
+        />
       </div>
     );
   }
