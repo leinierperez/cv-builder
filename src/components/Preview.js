a91cedd885311dc7eefc7e9fbd8b35ae256518b0
Leinier Perez <P_leinier@yahoo.com>
2022-12-14 13:47:36 -0500

    Finish preview component and add styles

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { FaPhoneSquareAlt } from 'react-icons/fa';
+import { GrMapLocation } from 'react-icons/gr';
+import { IoMdMail } from 'react-icons/io';
+import '../Preview.css';
 
 class Preview extends Component {
   constructor(props) {
@@ -11,39 +15,59 @@ class Preview extends Component {
       <div className='preview'>
         <div className='top-header'>
           <div className='left'>
-            <h1 className='firstName'>{generalInfo.name}</h1>
-            <h4 className='title'>{generalInfo.title}</h4>
+            <h1 className='name'>{generalInfo.name}John Doe</h1>
+            <h4 className='title'>{generalInfo.title}Software Engineer</h4>
           </div>
           <div className='right'>
-            <div>icon{generalInfo.phone}</div>
-            <div>icon{generalInfo.email}</div>
-            <div>icon{generalInfo.address}</div>
+            <div>
+              <FaPhoneSquareAlt />
+              {generalInfo.phone}564564564565
+            </div>
+            <div>
+              <IoMdMail />
+              {generalInfo.email}fhfg@fdg.com
+            </div>
+            <div>
+              <GrMapLocation />
+              {generalInfo.address}gjghj 657 fgh 8888
+            </div>
           </div>
         </div>
 
-        <div className='description'></div>
-        {this.props.experienceGroups.map((group) => {
-          return (
-            <div key={group.id} className='experience'>
-              <p>{group.data.companyName}</p>
-              <p>
-                {group.data.position} | {group.data.startDate} -{' '}
-                {group.data.endDate}
-              </p>
-            </div>
-          );
-        })}
-        {this.props.educationGroups.map((group) => {
-          return (
-            <div key={group.id} className='education'>
-              <p>{group.data.degree}</p>
-              <p>
-                {group.data.universityName} | {group.data.startDate} -{' '}
-                {group.data.endDate}
-              </p>
-            </div>
-          );
-        })}
+        <div className='description'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+          quae, eum libero quisquam cupiditate rerum nesciunt deserunt vel quia
+          ea obcaecati assumenda quos alias accusamus consequatur officia,
+          quibusdam totam molestias.
+        </div>
+        <div className='experience'>
+          <h2>Work Experience</h2>
+          {this.props.experienceGroups.map((group) => {
+            return (
+              <div key={group.id} className='group'>
+                <p>{group.data.companyName}</p>
+                <p>
+                  {group.data.position} | {group.data.startDate} -{' '}
+                  {group.data.endDate}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className='education'>
+          <h2>Education</h2>
+          {this.props.educationGroups.map((group) => {
+            return (
+              <div key={group.id} className='group'>
+                <p>{group.data.degree}</p>
+                <p>
+                  {group.data.universityName} | {group.data.startDate} -{' '}
+                  {group.data.endDate}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
